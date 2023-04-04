ff64ea190fc980799a44e300f9e38b695e575553
Christian Brandão Figueiredo <developer.figueiredo@gmail.com>
2023-04-04 11:47:14 -0300

    add crud

---
+++ SmartSchool.WebAPI/Controllers/AlunoController.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SmartSchool.WebAPI.Models;
+
+namespace SmartSchool.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlunoController : ControllerBase
+    {
+        public List<Aluno> Alunos = new List<Aluno>(){
+            new Aluno(1, "Marta", "Kent", "33225555"),
+            new Aluno(2, "Paula", "Isabela", "3354288"),
+            new Aluno(3, "Laura", "Antonia", "55668899"),
+        };
+        public AlunoController() { }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Alunos);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null) return BadRequest("O aluno não foi encontrado.");
+            return Ok(aluno);
+        }
+
+        [HttpGet("{nome}")]
+        public IActionResult GetByName(string nome)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Nome.Contains(nome));
+            if (aluno == null) return BadRequest("O aluno não foi encontrado.");
+            return Ok(aluno);
+        }
+
+        [HttpPost]  
+        public IActionResult Post(Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+       
+        
+    }
+}
