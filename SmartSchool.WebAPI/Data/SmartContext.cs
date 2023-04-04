80d21c91150be78197748973e28a0d4b60c30131
Christian Brandão Figueiredo <developer.figueiredo@gmail.com>
2023-04-04 13:42:47 -0300

    add context

---
+++ SmartSchool.WebAPI/Data/SmartContext.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SmartSchool.WebAPI.Models;
+
+
+namespace SmartSchool.WebAPI.Data
+{
+    public class SmartContext : DbContext 
+    {
+        public SmartContext(DbContextOptions<SmartContext> options) : base(options) { }
+
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Professor> Professores { get; set; }
+        public DbSet<Disciplina> Disciplinas { get; set; }
+        public DbSet<AlunoDisciplina> AlunoDisciplinas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AlunoDisciplina>()
+                .HasKey(AD => new { AD.AlunoId, AD.DisciplinaId });
+        }
+    }
+}
