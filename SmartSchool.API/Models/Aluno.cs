280bf1c87c608077e828cc61ec6ab32eeb140857
Christian Brandão Figueiredo <christian.figueiredo@vixteam.com.br>
2023-12-09 19:47:40 -0300

    add models

---
+++ SmartSchool.API/Models/Aluno.cs
@@ -0,0 +1,21 @@
+﻿namespace SmartSchool.API.Models
+{
+    public class Aluno
+    {
+        public Aluno(){} 
+
+        public Aluno(int id, string nome, string sobrenome, string telefone) 
+        {
+            this.Id = id;
+            this.Sobrenome = sobrenome;
+            this.Telefone = telefone;
+            this.Nome = nome;   
+        }
+        public int Id { get; set; }
+        public string Nome { get; set; } 
+        public string Sobrenome { get; set; }
+        public string Telefone { get; set; } 
+        public IEnumerable<AlunoDisciplina> AlunosDisciplinas { get; set; }       
+        
+    }
+}
---
+++ SmartSchool.API/Models/Disciplina.cs
@@ -0,0 +1,22 @@
+﻿namespace SmartSchool.API.Models
+{
+    public class Disciplina
+    {
+        public Disciplina() { }
+
+         public Disciplina(int id, string nome, int professorId) 
+        {
+            this.Id = Id;
+            this.Nome = nome;
+            this.ProfessorId = professorId;   
+        }
+
+        public 	int Id { get; set; }       
+        public string Nome { get; set; }
+        public int ProfessorId { get; set; }
+        public Professor Professor { get; set; }   
+
+        public IEnumerable<AlunoDisciplina> AlunosDisciplinas { get; set; } 
+            
+    }
+}
