063c48702e6ca1e37794f14c707c83901da118e8
Christian Brandão Figueiredo <christian.figueiredo@vixteam.com.br>
2023-12-11 21:16:21 -0300

    alteração nos models

--- SmartSchool.API/Models/Disciplina.cs
+++ SmartSchool.API/Models/Disciplina.cs
@@ -4,19 +4,22 @@
     {
         public Disciplina() { }
 
-        public Disciplina(int id, string nome, int professorId)
+        public Disciplina(int id, string nome, int professorId, int cursoId)
         {
             this.Id = Id;
             this.Nome = nome;
             this.ProfessorId = professorId;
+            this.CursoId = cursoId;
         }
-
         public int Id { get; set; }
         public string Nome { get; set; }
+        public int CargaHoraria { get; set; }
         public int ProfessorId { get; set; }
         public Professor Professor { get; set; }
-
+        public int? PrerequisitoId { get; set; } = null;
+        public Disciplina Prerequsito { get; set; }
+        public int CursoId { get; set; }
+        public Curso Curso { get; set; }        
         public IEnumerable<AlunoDisciplina> AlunosDisciplinas { get; set; }
-
     }
 }
